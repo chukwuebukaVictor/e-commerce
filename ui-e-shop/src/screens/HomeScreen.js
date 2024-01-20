9c8a7add5d69e88be522d992dd1696d103a99e06
Chukwuebuka <Chukwuebuka.victor.ozoede@gmail.com>
2024-01-20 10:02:57 +0100

    Style main with react bootstrap and setup footer

--- ui-e-shop/src/screens/HomeScreen.js
+++ ui-e-shop/src/screens/HomeScreen.js
@@ -1,13 +1,37 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import logger from 'use-reducer-logger';
 
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'FETCH_REQUEST':
+        return { ...state, loading: true };
+      case 'FETCH_SUCCESS':
+        return { ...state, products: action.payload, loading: false };
+      case 'FETCH_FAIL':
+        return { ...state, loading: false, error: action.payload };
+      default:
+        return state;
+    }
+  };
+  // const [products, setProducts] = useState([]);
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: '',
+  });
+
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('/api/products');
-      setProducts(result.data);
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (error) {
+        dispatch({ type: 'FETCH_FAIL', payload: error.message });
+      }
     };
     fetchData();
   }, []);
@@ -15,24 +39,30 @@ function HomeScreen() {
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {products.map((product) => (
-          <div className="product" key={product.slug}>
-            <div>
-              <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </Link>
+        {loading ? (
+          <div>loading ...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map((product) => (
+            <div className="product" key={product.slug}>
+              <div>
+                <Link to={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </Link>
+              </div>
+              <div className="product-info">
+                <Link to={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </Link>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
             </div>
-            <div className="product-info">
-              <Link to={`/product/${product.slug}`}>
-                <p>{product.name}</p>
-              </Link>
-              <p>
-                <strong>${product.price}</strong>
-              </p>
-              <button>Add to cart</button>
-            </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
--- ui-e-shop/src/screens/ProductScreen.js
+++ ui-e-shop/src/screens/ProductScreen.js
@@ -2,12 +2,12 @@ import { useParams } from 'react-router-dom';
 
 function ProductScreen() {
   const params = useParams();
-    const { slug } = params;
-    return (
-      <div>
-        <h1>{slug}</h1>;
-      </div>
-    );
+  const { slug } = params;
+  return (
+    <div>
+      <h1>{slug}</h1>
+    </div>
+  );
 }
 
 export default ProductScreen;
